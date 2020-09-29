d42f3d779225dc394847d4298a82d0ea507fde49
Flavio Rocha <flavio_271295@hotmail.com>
2020-09-29 19:55:11 -0300

    alteração teste

--- js/config.js
+++ js/config.js
@@ -1,7 +1,2 @@
-export const CEP_API_URL = "https://viacep.com.br/ws/"
-export const JSON_FORMAT = "/json"
-export const HTTP_STATUS_OK = 200
-export const GOOGLE_MAPS_GEOCODE_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/json?address="
-export const GOOGLE_API_KEY = "&key=AIzaSyDrM2JAFNh8SiW7pIEnvBcoj6JIEyFWpxo"
-export const MOCK_API_URL = "http://5c251c85cfdadd0014d14b4b.mockapi.io/api/address"
-export const currentAddressKey = "currentAddress"
+export const CEP_API_URL = "https://viacep.com.br/ws/";
+export const JSON_FORMAT = "/json";
